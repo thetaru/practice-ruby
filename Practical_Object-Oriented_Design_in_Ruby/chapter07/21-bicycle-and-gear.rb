36444ebce55bc8805f4ba089f072d2261b92dd45
thetaru <sensesensesense012345678910@gmail.com>
2022-12-10 02:12:58 +0900

    オブジェクト指向設計 実践ガイド chapter07 完

---
+++ Practical_Object-Oriented_Design_in_Ruby/chapter07/21-bicycle-and-gear.rb
@@ -0,0 +1,43 @@
+class Schedule
+    def scheduled?(schedulable, start_date, end_date)
+        puts "This #{schedulable.class} " +
+             "is not scheduled\n" +
+             " between #{start_date} and #{end_date}"
+        false
+    end
+end
+
+module Schedulable
+    attr_writer :schedule
+
+    def schedule
+        @schedule ||= ::Schedule.new
+    end
+
+    def schedulable?(start_date, end_date)
+        !scheduled?(start_date - lead_days, end_date)
+    end
+
+    def scheduled?(start_date, end_date)
+        schedule.scheduled?(self, start_date, end_date)
+    end
+
+    def lead_days
+        0
+    end
+end
+
+class Bicycle
+    include Schedulable
+
+    def lead_days
+        1
+    end
+end
+
+require 'date'
+starting = Date.parse("2015/09/04")
+ending = Date.parse("2015/09/10")
+
+b = Bicycle.new
+puts b.schedulable?(starting, ending)
